8595c4ab8c0cd54fabeafae12a85e8f25b845097
巨龙 <gaojulong@outlook.com>
2017-12-11 21:49:42 +0800

    更新游戏

--- app/src/main/java/cn/lovelqq/julong/voicerw/MainActivity.java
+++ app/src/main/java/cn/lovelqq/julong/voicerw/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Shibie shibie=new Shibie();
     public static String strRandom=null;//随机数
     public static int score=0;//所得分
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
